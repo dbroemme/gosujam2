384b7c5eed871cdd00df3785f9370254738f906e
dbroemme <darren.broemmer@gmail.com>
2022-02-08 09:06:42 -0500

    remove old files

--- lib/rdia-games.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "rdia-games/version"
-require_relative "rdia-games/widgets"
-require_relative "rdia-games/app"
--- lib/rdia-games/version.rb
+++
@@ -1,3 +0,0 @@
-module RdiaGames
-    VERSION = "0.1.0"
-end
